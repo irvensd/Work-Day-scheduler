8bfa92ddfdf1dfc350b5ae46e73d3afabfe35ce3
Irvens R Dupuy <92906651+irvensd@users.noreply.github.com>
2021-12-17 15:21:20 -0500

    Started adding the rows for the calendar

--- js/script.js
+++ js/script.js
@@ -1,20 +1,8 @@
-// var timeBLock = $('#time-block')
 
-// let timeBlock= document.getElementById("time-block"); 
-// console.log(timeBlock);
+// let rightNow = moment().format('ddd, MMM Do');
+// let timer = document.getElementById("timer");
+// timer.innerHTML = rightNow
 
-// timeBlock.textContent = "Hello Irvens";
-//  body.appendChild(timeBlock);
- 
-// let container = $('.container');
-
-// let timeBlock = $('#time-block'); 
-
-// timeBlock.text('Hello Irvens'); 
-
-// container.append(timeBlock); 
-
-var rightNow = moment().format('ddd, MMM Do');
-var timer = document.getElementById("timer");
-console.log(timer);
-timer.innerHTML = rightNow
+let rightNow = moment().format('ddd, MMM Do');
+let timer = $('#timer');
+timer.text(rightNow)
